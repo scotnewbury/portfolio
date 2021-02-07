67588d7545f35ff86963dab8492227ee8c4400bd
Scot Newbury <scot@scotnewbury.com>
2021-02-07 13:15:44 -0500

    Created assets folder

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,16 +4,17 @@ const HomePage = () => (
   <div>
     <div className="main">
       <div className="showcase">
-        <h2>Let's build something together.</h2>
+        
         <h1>Scot Newbury</h1>
         <h2>Web Developer</h2>
-        <div className="skillicons">
+        <h2>Let's build something together.</h2>
+        {/* <div className="skillicons">
           <i class="fab fa-html5"></i>
           <i class="fab fa-css3-alt"></i>
           <i class="fab fa-js"></i>
           <i class="fab fa-react"></i>
           <i class="fab fa-wordpress"></i>
-        </div>
+        </div> */}
       </div>
     </div>
   </div>
