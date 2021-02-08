a57f4a3e36a4595c884ea85265eb277b0cc7bb29
Scot Newbury <scot@scotnewbury.com>
2021-02-08 12:34:57 -0500

    Added second project to page

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Card from './CardUI';
-import img1 from '../../assets/images/stmarkslodge-website-screen-capture.png';
-import img2 from '../../assets/images/background-image.jpg';
+import img1 from '../../assets/images/stmarkslodge-theme.png';
+import img2 from '../../assets/images/personal-portfolio.png';
 import img3 from '../../assets/images/background-image.jpg';
 import './card-style.css';
 
@@ -14,8 +14,11 @@ class Cards extends Component {
           cardtext="Created a custom WordPress theme including a hero image." 
           siteurl="https://stmarkslodge.org"
           githubrepo="https://github.com/scotnewbury/st-marks-website-theme" />
-        <Card imgsrc={img2} />
-        <Card imgsrc={img3} />
+        <Card imgsrc={img2} title="Personal Portfolio"
+          cardtext="My portfolio created with React, React router, and a touch of bootstrap."
+          siteurl="https://portfolio.scotnewbury.com"
+          githubrepo="https://github.com/scotnewbury/portfolio" />
+        {/* <Card imgsrc={img3} /> */}
       </div>
     );
   }
