219cd847fc6dc882fd87b69364e0c2cc47ae16d9
Scot Newbury <scot@scotnewbury.com>
2021-02-09 00:42:17 -0500

    Changed project card order

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -10,14 +10,14 @@ class Cards extends Component {
   render() {
     return (
       <div className="card-container">
-        <Card imgsrc={img1} title="St. Mark's Lodge Website" 
-          cardtext="Created a custom WordPress theme including a hero image." 
-          siteurl="https://stmarkslodge.org"
-          githubrepo="https://github.com/scotnewbury/st-marks-website-theme" />
         <Card imgsrc={img2} title="Personal Portfolio"
           cardtext="My portfolio created with React, React router, and a touch of bootstrap."
           siteurl="https://portfolio.scotnewbury.com"
           githubrepo="https://github.com/scotnewbury/portfolio" />
+        <Card imgsrc={img1} title="St. Mark's Lodge Website" 
+          cardtext="Created a custom WordPress theme including a hero image." 
+          siteurl="https://stmarkslodge.org"
+          githubrepo="https://github.com/scotnewbury/st-marks-website-theme" />
         {/* <Card imgsrc={img3} /> */}
       </div>
     );
